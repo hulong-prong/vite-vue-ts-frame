4251ca472035fab6a1a91451f2f11f932eb81e1e
hulong <461677691@qq.com>
2022-11-29 15:31:56 +0800

    declare声明类型

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare interface Fn<T = any, R = T> {
+  (...arg: T[]): R
+}
+declare type TimeoutHandle = ReturnType<typeof setTimeout>
+declare type Recordable<T = any> = Record<string, T>
